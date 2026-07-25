6ff91de14773a1590c417c919b4bf2245372e1c2
sanjanaramesh0530 <sanjana.ramesh0530@gmail.com>
2026-07-25 11:16:50 +0530

    created file

---
+++ constant.c
@@ -0,0 +1,8 @@
+int main()
+{
+    const int num=45;
+    printf("%d\n", num);
+    /*int num=10; // This line will cause a compilation error because 'num' is declared as const*/
+    printf("%d", num); 
+    return 0;
+}
